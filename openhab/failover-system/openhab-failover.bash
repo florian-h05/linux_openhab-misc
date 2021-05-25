f82263bf6a419cb829c6e1e7566a41a1861c76da
Florian Hotze <fhgames18@icloud.com>
2021-05-25 22:39:09 +0200

    Update openhab-failover.bash for client cert auth
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>

--- openhab/failover-system/openhab-failover.bash
+++ openhab/failover-system/openhab-failover.bash
@@ -9,13 +9,16 @@
 basicAuth_username=""
 basicAuth_password=""
 hostname=""
-openhab_token=""
 
 # your docker container name and notification settings go here
 # for notification, this script relies on "signal-cli-rest-api_client.bash", also available at "www.github.com/florian-h05/linux_openhab-misc"
 container="openhab_openhab_1"
 notify="false"
+recipient="" # for notify
 path=""
+client_cert="true" # when using client cert auth, then set to "true", when not using client cert auth, then set to "false"
+client_certName="user.p12"
+CA_cert="yourca.crt"
 
 start_docker() {
     if sudo docker start ${container} >/dev/null 2>&1
@@ -50,24 +53,34 @@ send_Notification() {
     TEXT="\nPlease check your openHAB installation.\n\nA RUNNING container means that your normal openHAB is not reachable!!\nA NOT RUNNING container menas that your normal openHAB is reachable."
     if [ "${containerStart}" != "${CHECK}" ] && [ "${notify}" == "true" ]
     then
-# specify the recipient of the message
-        if bash "${path}"signal-cli-rest-api_client.bash send <recipient> "openHAB failover container\n\nThe container's state is: ${CHECK}.${TEXT}" >/dev/null 2>&1; then echo "SUCCESS: sent notifification."; else "ERROR: sending notifiation failed!" >&2; fi
+        if bash "${path}"signal-cli-rest-api_client.bash send "${recipient}" "openHAB failover container\n\nThe container's state is: ${CHECK}.${TEXT}" >/dev/null 2>&1; then echo "SUCCESS: sent notifification."; else "ERROR: sending notifiation failed!" >&2; fi
     fi
 }
 
+main() {
+    if ! ${1} >/dev/null 2>&1
+    then
+        containerStart=$(check_container)
+        echo "ERROR: openhab not reachable!" >&2
+        start_docker
+        send_Notification
+        exit 1
+    else
+        containerStart=$(check_container)
+        echo "SUCCESS: openhab installation is reachable."
+        stop_docker
+        send_Notification
+    fi
+}
+
+
 ## when using self-signed certs, you have two options:
 #     - use "--cacert" and store your caert in pem format in path
-#     - use "--insecure" to ignore self-signed certs 
-if ! curl -X GET "https://${basicAuth_username}:${basicAuth_password}@${hostname}/rest/" -H "accept: application/json" -H "X-OPENHAB-TOKEN: ${openhab_token}" --cacert "${path}"yourCA.crt >/dev/null 2>&1
+#     - use "--insecure" to ignore self-signed certs
+if [ "${client_cert}" == "true" ]
 then
-    containerStart=$(check_container)
-    echo "ERROR: openhab not reachable!" >&2
-    start_docker
-    send_Notification
-    exit 1
-else
-    containerStart=$(check_container)
-    echo "SUCCESS: openhab installation is reachable."
-    stop_docker
-    send_Notification
+    main "curl -X GET --cert-type P12 --cert ${path}${client_certName} https://${hostname}/rest/ --cacert ${path}${CA_cert}"
+elif [ "${client_cert}" == "false" ]
+then
+    main "curl -X GET https://${basicAuth_username}:${basicAuth_password}@${hostname}/rest/ --cacert ${path}${CA_cert}"
 fi
