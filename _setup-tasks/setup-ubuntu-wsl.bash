3f827050397ffadb0e7f7b7ea7498054e55794d3
Florian Hotze <florianh_dev@icloud.com>
2022-02-11 14:51:41 +0100

    Update script for development with Shellcheck & improvements
    Signed-off-by: Florian Hotze <florianh_dev@icloud.com>

--- _setup-tasks/setup-ubuntu-wsl.bash
+++ _setup-tasks/setup-ubuntu-wsl.bash
@@ -18,6 +18,15 @@ if (( EUID == 0 )); then
     exit
 fi
 
+printf "Welcome to the development-on-wsl.bash script. \nThis script will install the required packages and tools for Florian Hotze. \nScript output is green or red, while most output from executed commands is blue.\n"
+echo "Ready to start? (y/n)"
+read -r choice
+case "${choice}" in
+    n|N ) exit;;
+    y|Y ) ;;
+    * ) echo "invalid";;
+esac 
+
 unset command_not_found_handle
 
 run_with_echo() {
@@ -31,7 +40,7 @@ echo "${reset}Updating package list ...${blue}"
 sudo apt update &> /dev/null
 
 # Install git.
-if ! command -v git &>/dev/null; then
+if ! command -v git; then
     echo "${reset}Installing git ...${blue}"
     run_with_echo "sudo apt install -y git" "Installed git." "Failed to install git."
 fi
@@ -50,7 +59,7 @@ configure_git() {
 
 echo "${red}Do you want to configure git? (y/n)"
 read -r choice
-case "$choice" in 
+case "${choice}" in 
     y|Y ) configure_git;;
     n|N ) ;;
     * ) echo "invalid";;
@@ -74,15 +83,18 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # Install latest LTS and openHAB-used NodeJS versions, use latest.
-echo "${green}Installing NodeJS versions ...${blue}"
+echo "${green}Installing NodeJS versions and tools ...${blue}"
 run_with_echo "nvm install --lts" "Installed NodeJS latest LTS version using nvm." "Failed to install NodeJS latest LTS version using nvm!"
-run_with_echo "npm install -g semistandard &>/dev/null" "Installed semistandard using npm." "Failed to install semistadard using npm."
+if ! command -v semistandard; then run_with_echo "npm install -g semistandard &>/dev/null" "Installed semistandard using npm." "Failed to install semistadard using npm."; fi
 run_with_echo "nvm install 12.16.1" "Installed NodeJS version 12.16.1 using nvm." "Failed to install NodeJS version 12.16.1 using nvm!"
-run_with_echo "npm install -g semistandard &>/dev/null" "Installed semistandard using npm." "Failed to install semistadard using npm."
+if ! command -v semistandard; then run_with_echo "npm install -g semistandard &>/dev/null" "Installed semistandard using npm." "Failed to install semistadard using npm."; fi
 nvm use --lts
 
 # Install Java 11.
-run_with_echo "sudo apt install -y openjdk-11-jdk" "Installed Java 11 JDK (OpenJDK)." "Failed to install Java 11 JDK (OpenJDK)!"
+if ! command -v java; then run_with_echo "sudo apt install -y openjdk-11-jdk" "Installed Java 11 JDK (OpenJDK)." "Failed to install Java 11 JDK (OpenJDK)!"; fi
 
 # Install Apache Maven.
-run_with_echo "sudo apt install -y maven" "Installed Maven Build System." "Failed to install Maven Build System."
+if ! command -v mvn; then run_with_echo "sudo apt install -y maven" "Installed Maven Build System." "Failed to install Maven Build System."; fi
+
+# Install Shellcheck
+if ! command -v shellcheck; then run_with_echo "sudo apt install -y shellcheck" "Installed Shellcheck (static code analysis for shell)." "Failed to install Shellcheck (static code analysis for shell)."; fi
