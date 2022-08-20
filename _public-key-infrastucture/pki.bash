5304418336a179b29f654fcc6653271c67bfa094
Florian Hotze <florianh_dev@icloud.com>
2022-08-20 20:39:39 +0200

    [pki] Update script

--- _public-key-infrastucture/pki.bash
+++ _public-key-infrastucture/pki.bash
@@ -21,34 +21,44 @@ mkdir -p pki/private/keys
 mkdir -p pki/p12
 mkdir -p tmp
 
+require_cn() {
+  if [ "${commonName}" == "" ]; then
+    echo "Provide command line argument -cn or --commonname"
+    exit 1
+  fi
+}
+
 generate_ecc_key() {
+  require_cn
   echo -e "\nGenerating ECC key ...\n"
   openssl ecparam -out "pki/private/keys/${commonName}.key" -name secp521r1 -genkey
 }
 
 generate_rsa_key() {
+  require_cn
   echo -e "\nGenerating RSA key ...\n"
   openssl genrsa -out "pki/private/keys/${commonName}.key" 4096
 }
 
 generate_client_csr() {
+  require_cn
   echo -e "\nGenerating CSR ...\n"
-  openssl req -new -x509 -days 1825 -key "pki/private/keys/${commonName}.key" -out "tmp/${commonName}.req" -addext "${OVPN_CLIENT_KEY_USAGE}" -addext "${OVPN_CLIENT_EXT_KEY_USAGE}" -subj \
-  "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${organizationalUnit}/CN=${commonName}"
+  openssl req -new -x509 -days 1825 -key "pki/private/keys/${commonName}.key" -out "tmp/${commonName}.req" -addext "${OVPN_CLIENT_KEY_USAGE}" -addext "${OVPN_CLIENT_EXT_KEY_USAGE}" -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${organizationalUnit}/CN=${commonName}"
 }
 
 generate_ovpn_server_csr() {
   echo -e "\nGenerating OpenVPN server CSR ...\n"
-  openssl req -new -x509 -days 1825 -key "pki/private/keys/${commonName}.key" -out "tmp/${commonName}.req" -addext "${OVPN_SERVER_KEY_USAGE}" -addext "${OVPN_SERVER_EXT_KEY_USAGE}" -subj \
-  "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=$VPN/CN=${commonName}"
+  openssl req -new -x509 -days 1825 -key "pki/private/keys/${commonName}.key" -out "tmp/${commonName}.req" -addext "${OVPN_SERVER_KEY_USAGE}" -addext "${OVPN_SERVER_EXT_KEY_USAGE}" -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${organizationalUnit}/CN=${commonName}"
 }
 
 import_csr() {
+  require_cn
   echo -e "\nImporting CSR ...\n"
   ./easyrsa import-req "tmp/${commonName}.req" "${commonName}"
 }
 
 sign_csr() {
+  require_cn
   echo -e "\nSigning CSR ...\n"
   ./easyrsa sign-req client "${commonName}"
 }
@@ -58,16 +68,19 @@ generate_crl() {
 }
 
 revoke_cert() {
+  require_cn
   ./easyrsa revoke "${commonName}"
   generate_crl
 }
 
 generate_p12() {
+  require_cn
   echo -e "\nGenerating p12 bundle ...\n"
   openssl pkcs12 -export -out "pki/p12/${commonName}.p12" -inkey "pki/private/keys/${commonName}.key" -in "pki/issued/${commonName}.crt"
 }
 
 generate_client_p12() {
+  require_cn
   generate_rsa_key
   #generate_ecc_key # TLS Handshake fails with secp521r keys!
   generate_client_csr
@@ -93,7 +106,6 @@ help() {
   echo "Command line args are:"
   echo "  -cn=* --commonname=*             common name for client certificate"
   echo "  -ou=* --organizationalunit=*     organizational unit (OU) for client certificate"
-  echo "                                   multiple values separated by point ."
   echo "  -e=*  --exec=*                   command to run"
   echo "Valid commands are:"
   echo "  generate_client_p12"
@@ -122,19 +134,9 @@ do
   esac
 done
 
-if [ "$exec" != "" ]; then
-  if [ "$exec" != "generate_ovpn_server" ]; then
-    if [ "$commonName" == "" ]; then
-      echo "Provide command line arg -cn"
-      exit 0
-    fi
-
-    if [ "$exec" == "" ]; then
-      echo "Provide command line arg -e"
-      exit 0
-    fi
-    "$exec"
-  else
-    "$exec"
-  fi
+if [ "${exec}" == "" ]; then
+  echo "Provide command line argument -e or --exec"
+  exit 1
 fi
+
+${exec}
